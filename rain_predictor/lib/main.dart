0053df9f03198bab67f3ba57415d86e5a0d894c2
EmmanuelMess <emmanuelbendavid@gmail.com>
2021-01-08 14:31:14 -0300

    Removed unused code

--- rain_predictor/lib/main.dart
+++ rain_predictor/lib/main.dart
@@ -10,6 +10,11 @@ import 'package:rain_predictor/forecast.dart';
 
 const FORECAST_URL = "https://ws1.smn.gob.ar/v1/forecast/location/";
 
+const String channel = "SINGLE";
+const String channel_name = "Default channel";
+const String channel_description = "Single channel";
+const int id = 0;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -60,8 +65,6 @@ Future<Forecast> fetchForecast(int days) async {
   if (response.statusCode == 200) {
     return Forecast.fromJson(json.decode(response.body));
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
@@ -150,24 +153,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Predictor de Lluvia',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(
-          _flutterLocalNotificationsPlugin,
-          _platformChannelSpecifics,
           title: 'Configuración del Predictor de Lluvia',
       ),
     );
@@ -175,59 +164,25 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin;
-  final NotificationDetails _platformChannelSpecifics;
-
-  MyHomePage(
-      this._flutterLocalNotificationsPlugin,
-      this._platformChannelSpecifics,
-      {Key key, this.title}
-      ) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState(_flutterLocalNotificationsPlugin, _platformChannelSpecifics);
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-const String channel = "SINGLE";
-const String channel_name = "Default channel";
-const String channel_description = "Single channel";
-const int id = 0;
-
 class _MyHomePageState extends State<MyHomePage> {
-  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin;
-  final NotificationDetails _platformChannelSpecifics;
 
-  _MyHomePageState(this._flutterLocalNotificationsPlugin, this._platformChannelSpecifics) : super();
-
-  int _counter = 0;
+  _MyHomePageState() : super();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
